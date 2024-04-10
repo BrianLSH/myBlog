e933d4b825c42cd81fd449810562a5240e9b7460
shanlirenneversaynever <shanlirenneversaynever@gmail.com>
2024-04-10 12:46:52 +0800

    css优化

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -77,7 +77,7 @@ export default {
                     ]
                 }
             ],
-            '/Html/': [
+            '/HtmlCss/': [
                 {
                     items: [
                         { text: 'html', link: '/HtmlCss/' },
